1bcf6a2c5b0f3c903177f200e62e1b52a7145419
spiffy <pravien.mangal@gmail.com>
2019-02-10 16:38:26 +0000

    Header component created

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>This is Unit Testing Practise</h1>
-        </header>
+        <Header />
       </div>
     );
   }
---
+++ to-do-app/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header className="App-header">
+      <h1>This is Unit Testing Practise</h1>
+    </header>
+  );
+}
+
+export default Header;
